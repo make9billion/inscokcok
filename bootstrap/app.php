7f3a15ddf2f373e9f5b176bc407a09d1072e32f8
make9billion <make9billion@gmail.com>
2026-06-11 18:59:33 +0900

    Localize Laravel error messages

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Session\TokenMismatchException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,5 +21,13 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (TokenMismatchException $exception, Request $request) {
+            $message = '로그인 세션이 만료되었습니다. 페이지를 새로고침한 뒤 다시 시도해 주세요.';
+
+            if ($request->expectsJson()) {
+                return response()->json(['message' => $message], 419);
+            }
+
+            return back()->withErrors(['csrf' => $message])->withInput();
+        });
     })->create();
